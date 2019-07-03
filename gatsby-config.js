b2503a4c677d80471292a8dd1b73a85960b2cad2
Gal Oppenheimer <gal@galbutnotgirl.com>
2019-07-03 13:33:31 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,9 @@ module.exports = {
     {
       resolve: 'gatsby-source-contentstack',
       options:{
-        'api_key':'your_api_key',
-        'access_token':'your_access_token',
-        'environment':'your_development'
+        'api_key':'blt959fc26d90dbf5c3',
+        'access_token':'cs81e2fcbf04337cba65a62c1e',
+        'environment':'preview'
       },
     },
     'gatsby-plugin-react-helmet'
